69111a9103bbb0d2768f9ea57b654d122d261ae6
aviv deri <aviv322deri@gmail.com>
2026-06-23 12:09:21 +0300

    exercise4

---
+++ array_methods.js
@@ -0,0 +1,10 @@
+2.
+const word = ["hello", "world"]
+word.map((item, index) => word[index] = item.toUpperCase())
+console.log(word)
+
+4.
+const num = [1, 2, 3, 4, 5, 6]
+const arr = num.filter(num => num % 2 === 0)
+console.log(arr)
+
