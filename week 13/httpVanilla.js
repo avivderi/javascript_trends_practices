b52e0fa5b332256ab0136fde99314534086f27a6
aviv deri <aviv322deri@gmail.com>
2026-07-05 17:04:45 +0300

    add vanilla

--- week 13/httpVanilla.js
+++ week 13/httpVanilla.js
@@ -1,14 +1,54 @@
-import http from "http"
+import http, { STATUS_CODES } from "http"
 
+// 1.
+// const server = http.createServer((req, res) => {
+//     if (req.url === "/Morning") {
+//         res.end("good Morning")
+//     } else if (req.url === "/Evening") {
+//         res.end("Good Evening")
+//     }
+// })
+
+// server.listen(3000, () => {
+//     console.log("Hello from my server")
+// })
+
+// 2.
+// const server = http.createServer((req,res) => {
+//     res.setHeader('Content-Type', 'text/plain');
+//     if (req.url === "/") {
+//         res.end("Home Page")
+//     } else if (req.url === "/about") {
+//         res.end("About Page")
+//     } else if (req.url === "/contact") {
+//         res.end("Contact Page")
+
+//     }else {
+//         res.statusCode = 404
+//         res.end("page not found")
+        
+//     }
+// })
+
+// server.listen(3000, () => {
+//     console.log("Hello from my server")
+// })
+
+3.
 const server = http.createServer((req, res) => {
-    if (req.url === "/users" && req.method === "GET") {
-        res.end("this is a users")
+    if (req.url === "/users") {
+        if (req.method === "GET") {
+            res.end("users list")
+        } else if (req.method === "POST") {
+            res.end("user created")
+        } else {
+            res.end("method not allowed")
+        }
     } else {
-        res.end("not found")
+        res.statusCode = 404
     }
 })
 
 server.listen(3000, () => {
-    console.log("server is runnig on port 3000")
+    console.log("Hello from my server")
 })
-
