fcb8a216bbf3f8c2daa4b125286733d595ab0022
aviv deri <aviv322deri@gmail.com>
2026-07-12 01:00:07 +0300

    refactor: simplify file read/write functions by removing error handling

--- week 13/project_server_of_store_online/utils/readAndWriteFiles.js
+++ week 13/project_server_of_store_online/utils/readAndWriteFiles.js
@@ -1,21 +1,12 @@
 import fs from 'fs/promises'
 
 export async function readFiles(filePath) {
-    try {
     const response = await fs.readFile(filePath, 'utf8')
     const data = JSON.parse(response)
     return data
-    } catch (error) {
-        return {success: false, message: error.message}
-    }
 }
 
 export async function writeFiles(filePath, content) {
-    try {
-        const response = await fs.writeFile(filePath, JSON.stringify(content))
-        return {success: true, data: 'successfully'}
-    } catch (error) {
-        return {success: false, message: error.message}
-    }
+    await fs.writeFile(filePath, JSON.stringify(content, null, 2))
+    return true
 }
-
