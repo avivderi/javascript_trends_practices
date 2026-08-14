33e32d0724effcd7c96acf0ddcda985e1811af00
aviv deri <aviv322deri@gmail.com>
2026-08-14 16:10:10 +0300

    feat: add Express server setup with API endpoint for products

---
+++ week 17/project kiosk/server/server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import { nextTick } from 'node:process'
+
+const app = express()
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.method(), res.path())
+    next()
+})
+
+app.get('/api/products', (req, res) => {
+    return res.json([{
+        id: 1,
+        name: 'milk',
+        price: 7.5,
+        category: "Dairy products"
+    },
+    {
+        id: 2,
+        name: 'cheese',
+        price: 9.5,
+        category: "Dairy products"
+    },
+    {
+        id: 3,
+        name: 'Delicacies',
+        price: 16.9,
+        category: "Dairy products"
+    },
+    ])
+})
+
+app.listen(3000, () => console.log('the server is running...'))
